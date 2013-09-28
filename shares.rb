42a2163e1116640d6e7d2a2befe021042a2f811e
Sam Soffes <sam@soff.es>
2013-09-28 11:47:04 -0700

    Header Not in love with it, but it works for now

--- shares.rb
+++ shares.rb
@@ -7,8 +7,8 @@ class Shares < Sinatra::Base
       APP_STORE_URL
     end
 
-    def app_store_button
-      %Q{<a href="#{APP_STORE_URL}" class="app-store" rel="external nofollow"><img src="/assets/app-store.svg" alt="Download on the App Store"></a>}
+    def app_store_button(white = false)
+      %Q{<a href="#{APP_STORE_URL}" class="app-store" rel="external nofollow"><img src="/assets/app-store#{white ? '-white' : ''}.svg" alt="Download on the App Store"></a>}
     end
   end
 
